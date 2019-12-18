1f19552b877db69ab6161e26c09092ac67429431
Anand Nayak <54777475+Andy487@users.noreply.github.com>
2019-12-18 12:48:30 +0530

    Add files via upload

---
+++ QUESTION.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+precentage ()
+{
+  float a,b;
+  printf("enter digit");
+  scanf("%f",&a);
+  b=(a*25)/100;
+  printf("%f in 25 of given digit",b);
+}
+percen()
+{
+   int a,b;
+   printf("enter value");
+   scanf("%d",&a);
+   b=(a*25)/100;
+   printf("%d is 25 percentage",b);
+   }
+   main()
+   {
+   if(a==int)
+   percentage();
+   else
+   percen();
+  getch();
+}
